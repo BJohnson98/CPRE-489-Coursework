a2b21b5d37b4bd1812d38df94dc44997cc44862c
Brandon Johnson <47310376+BJohnson98@users.noreply.github.com>
2020-01-30 13:31:11 -0600

    add sysinfo message

--- Lab2/server.c
+++ Lab2/server.c
@@ -1,23 +1,35 @@
-#include <sys/types.h>
+#include <stdio.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <sys/sysinfo.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
 struct sockaddr_in serveraddr, clientaddr;
-
 
 int main(){
 	int sersock, consock;
 	int len = sizeof(clientaddr);
 	char IP_ADDRESS[16] = "127.0.0.1";
 	int server_port = 9388;
-	char message[128] = "mission success";
+	char message[128];
 	
 	struct sysinfo s_info;
 
-	sprintf(message,"%ld seconds sinceboot ", s_info.uptime);
+    int time = s_info.uptime;
+    //convert time to seconds days, hours, minutes, and seconds
+	int days = time / ( 24*3600);
+	int hours = time % (24*3600)/3600;
+    int minutes = (time % (24*3600*3600))/60;
+    int seconds = (time % (24*3600*3600*60))/60;
 	
-	printf("\n%s\n", message);
+	//put the system info into string called message
+	sprintf(message,"%s:",IP_ADDRESS);
+	if(days > 0){ sprintf(message,"%s %d days,",message, days); }
+	if(hours > 0){ sprintf(message,"%s %d hours,",message, hours); }
+	if(minutes > 0){ sprintf(message,"%s %d minutes,",message, minutes); }
+	if(seconds > 0){ sprintf(message,"%s %d seconds,",message, seconds); }	
+	// load averages
+	sprintf(message,"%s load average: %lu, %lu, %lu",message, loads[0], loads[1], loads[2]);
 	
 	//step one:
 	// pf_inet means communication through internet.
@@ -30,7 +42,7 @@ int main(){
 	//Step two:
 	// Specify endpoint address for server socket.
 	serveraddr.sin_family = PF_INET;
-	///specify port number, htons helps specify byte order.
+	//specify port number, htons helps specify byte order.
 	// htons() -> h = host to n= network s = short; 2bytes. htonl -> l = long; 4 bytes
 	serveraddr.sin_port = htons(server_port);
 	serveraddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
@@ -40,18 +52,16 @@ int main(){
 	// bind assigns address and port to socket.
 	// after this step we have the first 4 parameters.
 	int bind_status = bind(sersock, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
-	
+	//exit if socket fails to bind.
 	if(bind_status != 0){
 		printf("failed to bind to port\nexiting...\n");
 		return 0;
 	}
 	
-
-
 	//Step 4:
 	// create passive mode socket
 	int listen_status = listen(sersock, 10);
-	
+	//exit if listening fails
 	if(listen_status != 0){
 		printf("listening failed.\nexiting...\n");
 		return 0;	
@@ -64,10 +74,9 @@ int main(){
 		//the consock is created so we can connect to multiple clients at one time.
 		consock = accept(sersock, (struct sockaddr *) &clientaddr, &len);
 
-		//communication between server and client starts here	 
-		
+		//communication between server and client starts here	 		
 		write(consock,message,128);
-								 
+		
 		//communication between server and client ends here
 		close(consock);
 		break;
@@ -77,4 +86,3 @@ int main(){
 	close(sersock);
 	return 0;
 }
-

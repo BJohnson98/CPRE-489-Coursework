0153b6ca623b31c1480e5a4c244a660a4a1fabf9
cdishman <wildgopher.colin@gmail.com>
2020-02-19 14:49:02 -0600

    My branch of code

--- Lab4/Primary.c
+++ Lab4/Primary.c
@@ -8,10 +8,13 @@
 
 void primary(int sockfd, double ber) {
 	int readSize;
-    char msg[DATA_LENGTH], srvReply[150];
+	int i;
+	int win[3] = {0, 1, 2};
+	char msg[DATA_LENGTH], srvReply[150];
 	unsigned char packet[PACKET_SIZE];
 	unsigned int packetNum = 0;
-
+	struct packet_buffer buff[3];
+	int k	
     //keep communicating with server
     while(1)
     {
@@ -27,12 +30,24 @@ void primary(int sockfd, double ber) {
 		fgets(msg, DATA_LENGTH+1, stdin);
 		// secondary is waiting for a *packet*, so we should packetize what
 		// we're sending to it
-		buildPacket(packet, DATA_PACKET, msg, packetNum++);
+		// TODO - make a send window of size three packets
+		// Put the sent packets in a buffer until an ACK has been recieved
+		// DATA_OFFSET = 2
+
+		//Have a for loop to put three packets into the buffer
+		for(i = 0; i < 3; i++){
+			buildPacket(packet, DATA_PACKET, msg, packetNum++);
+			//buff[i].packet = packet;
+			//printfPacket(buff[i].packet[i]);
+		}
    
 		// 0 - Options and are not necessary here
 		// If return value < 0, an error occured
-        if( send(sockfd , packet, PACKET_SIZE, 0) < 0)
-            perror("Send failed");
+	//For loop to send three packets
+//	for(i = 0; i < 3; i++){
+        	if( send(sockfd , buff[i].packet, PACKET_SIZE, 0) < 0)
+            		perror("Send failed");
+//	}
          
         // Receive a reply from the server
 		// NOTE: If you have more data than 149 bytes, it will 
@@ -43,8 +58,22 @@ void primary(int sockfd, double ber) {
 		// 149 - the size of the receiving buffer (any more data will be 
 		// 		delievered in subsequent "recv" operations
 		// 0 - Options and are not necessary here
-        if( (readSize = recv(sockfd , srvReply , 149 , 0)) < 0)
-            perror("recv failed");
+	//For loop for receiving packets
+//	for(i = 0; i < 3; i++){
+        	if( (readSize = recv(sockfd , srvReply , 149 , 0)) < 0)
+            		perror("recv failed");
+//	}
+
+	//TODO
+	//svrReply is the packet we get back
+	//srvReply[0] would have ACK or NAK
+	//srvRelpy[1] would have the sequence number
+	//If the sequence number is correct 
+	//	Then we adjust the send window and removes packet from the buffer 
+	//	If something is wrong we will have to resend packets
+	//		To do this we will need to adjust send window and
+	//		retransmit all packets in send window
+	
 
 		// In general, secondary responds with only ACK or NAK *packets*
 		// so we should print them with printPacket(x)
@@ -53,5 +82,3 @@ void primary(int sockfd, double ber) {
     }
   
 }
-
-

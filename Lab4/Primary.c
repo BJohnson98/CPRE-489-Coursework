50cda55c5ebe2888d9f7f6fb6ede5fa0b1f0971f
cdishman <wildgopher.colin@gmail.com>
2020-02-29 10:35:42 -0600

    Ready to test

--- Lab4/Primary.c
+++ Lab4/Primary.c
@@ -8,13 +8,13 @@
 
 void primary(int sockfd, double ber) {
 	int readSize;
-	int i;
-	int win[3] = {0, 1, 2};
-	char msg[DATA_LENGTH], srvReply[150];
+	int index = 0;
+	int win[3] = {0,1,2};
+    	char msg[DATA_LENGTH], srvReply[150];
 	unsigned char packet[PACKET_SIZE];
+	struct packet_buffer buff[13];
 	unsigned int packetNum = 0;
-	struct packet_buffer buff[3];
-	int k	
+
     //keep communicating with server
     while(1)
     {
@@ -27,27 +27,56 @@ void primary(int sockfd, double ber) {
 		// ie if the user enters "HELLO<enter>", fgets will read in "HE", then
 		// packetize it and send it. The program will come back to fgets and
 		// read in "LL" and repeat until it sends "O"
-		fgets(msg, DATA_LENGTH+1, stdin);
+//		fgets(msg, DATA_LENGTH+1, stdin);
 		// secondary is waiting for a *packet*, so we should packetize what
 		// we're sending to it
-		// TODO - make a send window of size three packets
-		// Put the sent packets in a buffer until an ACK has been recieved
-		// DATA_OFFSET = 2
-
-		//Have a for loop to put three packets into the buffer
-		for(i = 0; i < 3; i++){
+//		buildPacket(packet, DATA_PACKET, msg, packetNum++);
+		
+		for(int i = 0; i < 13; i++){
+			fgets(msg, DATA_LENGTH+1, stdin);
 			buildPacket(packet, DATA_PACKET, msg, packetNum++);
-			//buff[i].packet = packet;
-			//printfPacket(buff[i].packet[i]);
+			//Introduce error
+//			IntroduceError(packet, ber);
+			buff[i] = packet;
 		}
-   
+		
+		while(1){
+			//Sends the packets 
+			for(int i = 0; i < 3; i++){
+				if( send(sockfd , buff[arr[i]], PACKET_SIZE, 0) < 0)
+					perror("Send failed");
+			}
+			
+			//Receives the packets
+			for(int i = 0; i < 3; i++){
+				if( (readSize = recv(sockfd , srvReply , 149 , 0)) < 0)
+					perror("recv failed");
+				printf("Server's reply:\n");
+				printPacket(srvReply);
+			}
+			switch(srvReply[1] - index){
+				case 0:
+					break;
+				case 1:
+					shiftwindow(win, 3, 1);
+					index++;
+					break;
+				case 2:
+					shiftwindow(win, 3, 2);
+					index += 2;
+					break;
+				case 3:
+					shiftwindow(win, 3, 3);
+					index += 3;
+				default:
+					printf("UNDEFINED");
+			}
+			
+		}
 		// 0 - Options and are not necessary here
 		// If return value < 0, an error occured
-	//For loop to send three packets
-//	for(i = 0; i < 3; i++){
-        	if( send(sockfd , buff[i].packet, PACKET_SIZE, 0) < 0)
-            		perror("Send failed");
-//	}
+//        if( send(sockfd , packet, PACKET_SIZE, 0) < 0)
+//            perror("Send failed");
          
         // Receive a reply from the server
 		// NOTE: If you have more data than 149 bytes, it will 
@@ -58,27 +87,14 @@ void primary(int sockfd, double ber) {
 		// 149 - the size of the receiving buffer (any more data will be 
 		// 		delievered in subsequent "recv" operations
 		// 0 - Options and are not necessary here
-	//For loop for receiving packets
-//	for(i = 0; i < 3; i++){
-        	if( (readSize = recv(sockfd , srvReply , 149 , 0)) < 0)
-            		perror("recv failed");
-//	}
-
-	//TODO
-	//svrReply is the packet we get back
-	//srvReply[0] would have ACK or NAK
-	//srvRelpy[1] would have the sequence number
-	//If the sequence number is correct 
-	//	Then we adjust the send window and removes packet from the buffer 
-	//	If something is wrong we will have to resend packets
-	//		To do this we will need to adjust send window and
-	//		retransmit all packets in send window
-	
+//        if( (readSize = recv(sockfd , srvReply , 149 , 0)) < 0)
+//            perror("recv failed");
 
 		// In general, secondary responds with only ACK or NAK *packets*
 		// so we should print them with printPacket(x)
-        printf("Server's reply:\n");
-		printPacket(srvReply);
+//        printf("Server's reply:\n");
+//		printPacket(srvReply);
     }
   
 }
+
